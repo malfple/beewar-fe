9c965f3c121d7cc570a815cd56ec863c13dfed09
Christopher <christopher.samuel@shopee.com>
2020-10-21 15:26:01 +0700

    Basic BE server call (ping)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
   return (
     <div className="App">
@@ -19,8 +21,32 @@ function App() {
           Learn React
         </a>
       </header>
+      <PingTest />
     </div>
   );
+}
+
+class PingTest extends React.Component {
+  handlePing(e) {
+    e.preventDefault()
+    console.log("ping!")
+    axios({
+      method: "GET",
+      url: "http://localhost:3001",
+    }).then(() => {
+      console.log("pong!")
+    }).catch(err => {
+      console.log("be server ded!")
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.handlePing.bind(this)}>
+        ping the server
+      </button>
+    )
+  }
 }
 
 export default App;
