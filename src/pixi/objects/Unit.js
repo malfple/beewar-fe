b274efd097fb5dffcad671ed19a57c6384e17c2c
Christopher <christopher.samuel@shopee.com>
2020-12-10 14:50:17 +0700

    refactor units

--- src/pixi/objects/Unit.js
+++ src/pixi/objects/Unit.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 import {infantryMaskTexture, infantryTintTexture, youMaskTexture, youTintTexture} from '../textures'
-import {UNIT_TYPE_INFANTRY, UNIT_TYPE_YOU} from '../../constants/unitConstants'
+import {PLAYER_COLOR_TINT, UNIT_TYPE_INFANTRY, UNIT_TYPE_YOU} from './unitConstants'
 
 class Unit {
   /**
@@ -36,15 +36,15 @@ class Unit {
     }
     let unitMaskSprite = new PIXI.Sprite(maskTexture)
     let unitTintSprite = new PIXI.Sprite(tintTexture)
+    unitTintSprite.tint = PLAYER_COLOR_TINT[owner]
     unitMaskSprite.anchor.set(0.5)
     unitTintSprite.anchor.set(0.5)
-    let y = 40 * i + 20
-    let x = 50 * j + (i % 2 === 0 ? 50 : 25)
-    unitMaskSprite.position.set(x, y)
-    unitTintSprite.position.set(x, y)
-    unitTintSprite.tint = (owner === 1 ? 0xFF5555 : 0x5555FF)
     this.pixiNode.addChild(unitMaskSprite)
     this.pixiNode.addChild(unitTintSprite)
+
+    let y = 40 * i + 20
+    let x = 50 * j + (i % 2 === 0 ? 50 : 25)
+    this.pixiNode.position.set(x, y)
   }
 }
 
