3b56c7918800a187ba591e531a1a58126f950564
Christopher Samuel <noogai886@yahoo.com>
2021-04-28 11:26:55 +0800

    update how2play

--- src/view/How2Play.js
+++ src/view/How2Play.js
@@ -6,7 +6,8 @@ function How2Play() {
       <h1>How to Play</h1>
       <h2>Login and Register</h2>
       <p>
-        Register with a unique email and username. After that, you can login
+        Register with a unique email and username. After that, you can login. <br />
+        <i>Note that because this is still in testing, the email is not verified and you can freely use any email.</i>
       </p>
       <h2>Initiating games</h2>
       <p>
@@ -19,9 +20,12 @@ function How2Play() {
       <p>
         Each player takes turns moving their unit.
         To know whose turn it is, see the map border. The border color will indicate the current player.
-        You can select units to check their movement range, indicated by green hexes.
+        You can select units (left click) to check their movement range, indicated by green hexes.
         When it's your turn, you can move the unit to an empty reachable hex and click again to confirm the move.
         To attack, move into attack range of an enemy unit and some red hexes will show.
+      </p>
+      <p>
+        You can hold right click to navigate the map.
       </p>
     </div>
   )
