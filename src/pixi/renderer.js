385cb45634b4bda51a44459f9bbd840c9053a9af
Christopher Samuel <noogai886@yahoo.com>
2021-04-29 18:44:24 +0800

    update terrain hex and reduce map width to 95%

--- src/pixi/renderer.js
+++ src/pixi/renderer.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 
-const renderer = new PIXI.Renderer({width: window.innerWidth, height: 0.75 * window.innerHeight, backgroundColor: 0x696969})
+const renderer = new PIXI.Renderer({width: 0.95 * window.innerWidth, height: 0.75 * window.innerHeight, backgroundColor: 0x696969})
 renderer.view.oncontextmenu = e => e.preventDefault()
 
 export {
