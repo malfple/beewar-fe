dd6d63cf673b79c4f25520de6b6e3aac74af0b16
Christopher Samuel <noogai886@yahoo.com>
2021-07-02 10:45:23 +0800

    show create game button in map view even if not logged-in

--- src/view/map/MapView.js
+++ src/view/map/MapView.js
@@ -1,16 +1,14 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useParams} from 'react-router-dom'
 
 import {apiMapGet} from '../../modules/api/map'
 import MapViewApp from '../../components/pixiapp/MapViewApp'
-import {UserTokenContext} from '../../context'
 import NormalLoadingSpinner from '../../components/loading/NormalLoadingSpinner'
 import Button from '../../components/forms/button/Button'
 
 function MapView() {
   const [map, setMap] = useState(null)
   const {id} = useParams()
-  const userToken = useContext(UserTokenContext)
 
   useEffect(() => {
     apiMapGet(id).then(res => {
@@ -20,17 +18,6 @@ function MapView() {
     })
 
   }, [id])
-
-  let createGameButton = null
-  if(userToken.username !== '') {
-    createGameButton = (
-      <Button theme="hollow" small={true}>
-        <Link to={`/game/create/${id}`}>
-          Create a game using this map
-        </Link>
-      </Button>
-    )
-  }
 
   if(!map) {
     return (
@@ -51,7 +38,11 @@ function MapView() {
         <h1>{map.name}</h1>
         <div>Size (width x height): {map.width} x {map.height}</div>
         <div>
-          {createGameButton}
+          <Button theme="hollow" small={true}>
+            <Link to={`/game/create/${id}`}>
+              Create a game using this map
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
